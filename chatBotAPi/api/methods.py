b14a7943f37f0cc58f75c6a3518003f39df2d05c
Pulkit Pagare <pulkit.pagare@valiancesolutions.com>
2017-10-05 16:22:42 +0530

    add on feature of check eligibilty

--- chatBotAPi/api/methods.py
+++ chatBotAPi/api/methods.py
@@ -1,5 +1,6 @@
 from api import tasks
 from api import constants
+from fuzzywuzzy import process
 
 
 def process_request(text, session_id):
@@ -22,6 +23,24 @@ def process_request(text, session_id):
         if action_complete and action == "find-scholarship":
             search_result = tasks.call_search_api(api_ai_response)
             scholarships_list = search_result['response']['scholarships']
+        if action_complete and action == "check-eligibility":
+            count = 0
+            scholarship = result['parameters']['scholarship']
+            religion = result['parameters']['religion']
+            scholarship_class = result['parameters']['class']
+            gender = result['parameters']['gender']
+            actual_scholarship, actual_scholarship_rules = get_scholarship_info(scholarship)
+            for rules in actual_scholarship_rules:
+                if religion or "For All" in rules['rulename']:
+                    count += 1
+                if scholarship_class in rules['rulename']:
+                    count += 1
+                if gender in rules['rulename']:
+                    count += 1
+            if count == 3:
+                speech = "You are Eligible for {scholarship_title}".format(scholarship_title=actual_scholarship)
+            else:
+                speech = "You are Not Eligible for {scholarship_title}".format(scholarship_title=actual_scholarship)
         return {
             "scholarships": scholarships_list,
             "options": options_list,
@@ -46,3 +65,29 @@ def get_options(contexts_name_list, action):
         all_options = constants.OPTIONS.get("start_up")
 
     return list(all_options)
+
+
+def get_scholarship_info(scholarship):
+    scholarship_auth_token = tasks.call_auth_api()
+    data_list = tasks.call_scholarships_api(scholarship_auth_token)
+    scholarship_dict = create_scholarship_dict(data_list)
+    actual_scholarship = (process.extractOne(scholarship, scholarship_dict.keys()))[0]
+    actual_scholarship_rules = create_scholarship_rules(actual_scholarship, data_list)
+    # if actual_scholarship in scholarship_dict:
+    #     scholarship_nid = scholarship_dict[actual_scholarship]
+    #     scholarship_detail = tasks.call_scholarship_detail_api(scholarship_nid,scholarship_auth_token)
+    return actual_scholarship, actual_scholarship_rules
+
+
+def create_scholarship_dict(data_list):
+    scholarship_dict = dict()
+    for scholarship in data_list:
+        scholarship_dict[scholarship['TITLE']] = scholarship['NID']
+    return scholarship_dict
+
+
+def create_scholarship_rules(actual_scholarship, data_list):
+    scholarship_rules = []
+    if actual_scholarship in data_list:
+        scholarship_rules = data_list['SCHOLARSHIP_RULES']
+    return scholarship_rules
--- chatBotAPi/api/tasks.py
+++ chatBotAPi/api/tasks.py
@@ -26,3 +26,33 @@ def call_search_api(data):
         return response.json()
     except Exception:
         return None
+
+
+def call_auth_api():
+    try:
+        url = "{auth_api}".format(auth_api=get_secret("AUTH_API"))
+        response = requests.post(url)
+        return response.json()['BODY']['DATA']['TOKEN']
+    except Exception:
+        return None
+
+
+def call_scholarships_api(scholarship_auth_token):
+    try:
+        url = "{scholarship_api_url}".format(scholarship_api_url=get_secret("SCHOLARSHIP_API_URL"))
+        headers = {'B4SAUTH': scholarship_auth_token}
+        response = requests.post(url, headers=headers)
+        return response.json()['BODY']['DATA']
+    except Exception:
+        return None
+
+
+def call_scholarship_detail_api(scholarship_nid, scholarship_auth_token):
+    try:
+        url = "{scholarship_detail_api_url}/{nid}".format(
+            scholarship_detail_api_url=get_secret("SCHOLARSHIP_DETAIL_API_URL"), nid=scholarship_nid)
+        headers = {'B4SAUTH': scholarship_auth_token}
+        response = requests.get(url, headers=headers)
+        return response.json()['BODY']['RECOMENDED'][0]
+    except Exception:
+        return None
